432ad0a3fc34b4153e0dc8204b4ef0dc2ecbb8a3
Matt Ford <mattford@users.noreply.github.com>
2022-12-01 15:53:34 +0000

    2020/3

--- pkg/year2020/year2020_test.go
+++ pkg/year2020/year2020_test.go
@@ -41,3 +41,20 @@ func Benchmark2020Day02(b *testing.B) {
 		}
 	})
 }
+func Benchmark2020Day03(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2020, 3)
+	p := aoc.NewPuzzle(2020, 3)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
